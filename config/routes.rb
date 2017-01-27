5f9a21049dd961e65c1647a78753d111882a7f11
Daniel Gonzalez <daniglezl@yahoo.com>
2017-01-27 15:33:01 -0500

    Modify routes and add login and other stuff

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,17 @@ Rails.application.routes.draw do
 
   root to: 'statics#home'
 
-  devise_for :users
+  as :user do
+    # sessions
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+
+    #registrations
+    get 'users/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration
+    get :signup, to: 'devise/registrations#new', as: :new_user_registration
+    get :profile, to: 'devise/registrations#edit', as: :edit_user_registration
+  end
+  devise_for :users, skip: [:sessions]
+
 end

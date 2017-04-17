d9bf962c5f8723ac4fc3c5978f6cda22e35e2f7c
Daniel Gonzalez <daniglezl@yahoo.com>
2017-04-17 08:24:53 -0400

    Restrict forums and comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,11 @@ class Ability
       can :manage, EventInstance, event: { user_id: user.id }
       can :manage, Attachment, event: { user_id: user.id }
       can :show, Attachment
+      can :manage, ForumThread, event: { user_id: user.id }
+      can :manage, ForumThread, user_id: user.id
+      can :manage, Comment, forum_thread: { event: { user_id: user.id } }
+      can :manage, Comment, forum_thread: { user_id: user.id }
+      can :manage, Comment, user_id: user.id
     end
   end
 end
